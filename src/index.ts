cc5dd244da74f2ef548dc4f9f399e12002c2a4ad
João Neto <joaodeveloper29@gmail.com>
2023-01-28 00:16:53 -0300

    chore: eslint, tsconfig.json and update package.json

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ import { join } from 'path'
 import autoLoad from '@fastify/autoload'
 
 const fastify = Fastify({
-    logger: true
+  logger: true
 })
 
 fastify.register(autoLoad, {
-    dir: join(__dirname, 'routes')
+  dir: join(__dirname, 'routes')
 })
 
 const start = async () => {
-    try {
-        await fastify.listen({ port: 3000 })
-    } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
 
 start()
--- src/routes/home.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from 'fastify'
-
-const home: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/home', async function (request, reply) {
-    return { home: true }
-  })
-}
-
-export default home
