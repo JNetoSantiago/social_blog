2eb114f67f6662447defc8a0dcbdaf69b58a6d64
João Neto <joaodeveloper29@gmail.com>
2023-01-28 15:30:09 -0300

    chore: adding lint staged

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ Kubernets
 
 Eslint
 Prettier
+
+Dia 28/01/2023
+* Instalei fastify e configurei o autoload nas rotas
+* Instalei e configurei typescript
+* configurei esbuild e nodemon
+* adicionei eslint
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,7 +1,7 @@
 import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get('/', async function () {
+  fastify.get('/', async function (add) {
     return { root: true }
   })
 }
